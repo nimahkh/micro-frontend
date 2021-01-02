049d7102798ce677135df129e0c8f0534bfc4e9d
nima habibkhoda <nima.habibkhoda>
2021-01-02 21:12:24 +0300

    remove comnsole

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ import render from "./render/ReactRender";
 render({ loading: true });
 
 const loader = (loading) => render({ loading });
-console.log("OK");
 registerMicroApps([
   {
     name: "mf-product", // app name registered
@@ -19,10 +18,7 @@ registerMicroApps([
     activeRule: "/mf-product",
   },
 ]);
-console.log("NOOOOO");
 prefetchApps([{ name: "mf-product", entry: "//locahost:7101" }]);
-console.log("Start");
 start();
-console.log("STARTEDDDD");
 setDefaultMountApp("/mf-product");
 addGlobalUncaughtErrorHandler((event) => console.log(event));
